b1e8bd8f8405ba09be3e436baa513816c3938ed1
Raza Mehdi <srmk@outlook.com>
2020-09-05 21:25:19 +0500

    Update build config & refactor tests for v3.

--- tests/Feature/AdapterFeatureTest.php
+++ tests/Feature/AdapterFeatureTest.php
@@ -21,7 +21,7 @@ class AdapterFeatureTest extends TestCase
     /** @var \Srmklive\PayPal\Services\PayPal */
     protected $client;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->client = new PayPalClient($this->getApiCredentials());
 
@@ -134,7 +134,7 @@ class AdapterFeatureTest extends TestCase
 
         $response = $this->client->listInvoices();
 
-        $this->assertNotEmpty($response);
-        $this->assertArrayHasKey('invoices', $response);
+        $this->assertArrayHasKey('total_pages', $response);
+        $this->assertArrayHasKey('total_items', $response);
     }
 }
